2b5bf6506d80280d1a1a8c808047e01fb3254a0f
JP Kramer <jpkramer707_github@mm.st>
2019-11-16 06:34:50 +0000

    Still no react router. Added a good Typeahead component. Supporting adding a new node based on user input.

--- src/Article.js
+++ src/Article.js
@@ -1,19 +1,65 @@
 import React from 'react';
-import Autocomplete from './Autocomplete';
+import { Typeahead } from 'react-bootstrap-typeahead';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
 
 class Article extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      value: ''
+      inputString: undefined
     };
+  }
+
+  onKeyDown(e) {
+    if (e.keyCode === 13) {
+      this.submitForm();
+    }
+  }
+
+  onChange(selected) {
+    var inputString;
+    switch(typeof selected) {
+      case 'object':
+        [{text: inputString}] = selected;
+      break;
+      case 'string':
+        inputString = selected;
+      break;
+    }
+    this.setState({inputString});
+    console.log('onChange: ', inputString);
+  }
+
+  submitForm() {
+    const submittedNode = this.getOrCreateNodeByText( this.state.inputString );
+    console.log('Submitted: ', submittedNode);
+  }
+
+  getOrCreateNodeByText(text) {
+    return this.getNodeByText(text) || this.createNode(text);
+  }
+
+  createNode(text) {
+    const newId = Math.max.apply( Math, this.props.conversation.nodes.map( node => node.id ) ) + 1;
+    const newNode = {
+      id: newId,
+      authorId: 1,
+      text: text,
+      parentId: this.props.nodeId,
+    };
+    this.props.conversation.nodes.push(newNode);
+    return newNode;
+  }
+
+  getNodeByText(text) {
+    return this.props.conversation.nodes.find( node => node.text === text );
   }
 
   render() {
     const {nodeId, conversation} = this.props;
-    const getNodeById = id => conversation.nodes.filter(node => node.id === id)[0];
-    const getAuthorByNode = node => conversation.users.filter(user => user.id === node.authorId)[0];
+    const getNodeById = id => conversation.nodes.find(node => node.id === id);
+    const getAuthorByNode = node => conversation.users.find(user => user.id === node.authorId);
     const thisNode = getNodeById(nodeId);
     const children = conversation.nodes.filter(node => node.parentId === thisNode.id && node.authorId === thisNode.authorId);
     const choices = conversation.nodes.filter(node => node.parentId === thisNode.id && node.authorId !== thisNode.authorId);
@@ -23,23 +69,17 @@ class Article extends React.Component {
       <article style={{ color: "#"+author.color }} title={ author.name }>
         { thisNode.text }
         &nbsp;
-        <Autocomplete
-          getItemValue={(node) => node.text}
-          suggestionsMenuId="input-name-suggestions"
-          items={ choices }
-          value={this.state.value}
-          onSubmit={
-            () => {
-              console.log('onSubmit');
-            }
+        <Typeahead
+          id="nope"
+          multiple={ false }
+          emptyLabel={ false }
+          onKeyDown={ this.onKeyDown.bind(this) }
+          labelKey="text"
+          options={
+            choices
           }
-          onChange={e => {
-            this.setState({ value: e.target.value });
-          }}
-          onSelect={value => {
-            console.log('onSelect');
-            return this.setState({ value });
-          }}
+          onInputChange={ this.onChange.bind(this) }
+          onChange={ this.onChange.bind(this) }
         />
       </article>
     );
